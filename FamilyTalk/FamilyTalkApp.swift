a2fee0af6577760d383a314e18f81ab296288e18
BRIKSING <BRIKSING@yandex.ru>
2026-04-23 22:20:37 +0500

    Initial Commit

---
+++ FamilyTalk/FamilyTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTalkApp.swift
+//  FamilyTalk
+//
+//  Created by Dmitrii Gramoteev on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
